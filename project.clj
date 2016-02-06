a2f34d235224add631c1ee112c7bc0ddfb9ea4ab
dominionized <dominique.begin.0@gmail.com>
2016-02-06 15:29:52 -0500

    Update license and url

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject rms-wallpaper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A simple program to get some RMS goodness on your Gnome desktop."
+  :url "github.com/dominionized/rms-wallpaper"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [enlive "1.1.6"]]
   :main ^:skip-aot rms-wallpaper.core
